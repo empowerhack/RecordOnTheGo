48298a3fe303eb0236d2e0c0587d6a86a0907f16
Varun Ramraj <varun.ramraj@gmail.com>
2016-04-10 20:29:50 +0100

    Trying to fix collectstatic dependencies.

--- RecordOnTheGo/settings.py
+++ RecordOnTheGo/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'RecordOnTheGo.urls'
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'RecordOnTheGo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'ROTG_db.sqlite3'),
     }
 }
 
@@ -117,12 +118,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(os.path.dirname(PROJECT_ROOT), 'prototype'),
 ]
 
 # Simplified static file serving.
