a80ce04f3a087c7d9e9adf0e671d560adb560d0a
Varun Ramraj <varun.ramraj@gmail.com>
2016-04-10 21:47:08 +0100

    Registration and login stuff now works.

--- prototype_app/urls.py
+++ prototype_app/urls.py
@@ -4,7 +4,10 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+
+    url(r'^registration.html', views.registration, name='registration'),
+    url(r'^login.html', views.loginpage, name='loginpage'),
+    url(r'^', views.index, name='index'),
 
     #url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
--- prototype_app/views.py
+++ prototype_app/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 # VR: Creating a basic index page
 from django.http import HttpResponse
 
+def registration(request):
+    return render(request, 'registration.html')
+
+def loginpage(request):
+    return render(request, 'login.html')
+
 def index(request):
     return render(request, 'index.html')
     #return HttpResponse("hello world")
