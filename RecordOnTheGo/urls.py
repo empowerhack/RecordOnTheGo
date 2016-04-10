efd0cc0d0409f39f3ed8d9ee22c04b21c4b9f9a5
Varun Ramraj <varun.ramraj@gmail.com>
2016-04-10 22:06:36 +0100

    Switched url parsing order.

--- RecordOnTheGo/urls.py
+++ RecordOnTheGo/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'RecordOnTheGo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include('prototype_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('prototype_app.urls')),
 ]
