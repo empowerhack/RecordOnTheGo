63ad3f585045fe1b45a45e5eb59501dc7c543936
neraks <karenfish.uk@gmail.com>
2016-04-10 13:11:57 +0100

    Patient dashboard high fidelity prototype

--- prototype/Gruntfile.js
+++ prototype/Gruntfile.js
@@ -1,23 +1,44 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    cssmin: {
-  options: {
-    shorthandCompacting: false,
-    roundingPrecision: -1
-  },
-  target: {
-    files: {
-      'css/main.css': ['node_modules/bootstrap/dist/css/bootstrap.css', 'node_modules/bootstrap/dist/css/bootstrap-theme.css', 'css/screen.css']
-    }
-  }
-}
-  });
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'css/main.css': ['node_modules/bootstrap/dist/css/bootstrap.css', 'node_modules/bootstrap/dist/css/bootstrap-theme.css', 'css/screen.css']
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    { expand: true, src: ['node_modules/angular/angular.js'], dest: 'js/', filter: 'isFile', flatten: true },
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+
+                ],
+            },
+        },
+        watch: {
+            scripts: {
+                files: ['css/*.css'],
+                tasks: ['cssmin'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['cssmin', 'copy', 'watch']);
 
 };
